87017c4d3ef715b30cbc18731e1b14f3bca1cee9
amangoelfv <amangoel300@gmail.com>
2022-01-12 18:26:57 +0530

    linting changes

--- models/notif.js
+++ models/notif.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
-require('mongoose-type-url');
+require("mongoose-type-url");
 
 const newsSchema = new mongoose.Schema(
   {
-    
-        img: {
-          type: mongoose.SchemaTypes.Url,
-          required: true
-        },
-        link: {
-          type: mongoose.SchemaTypes.Url, 
-          required: true
-        },
-    
+    img: {
+      type: mongoose.SchemaTypes.Url,
+      required: true,
+    },
+    link: {
+      type: mongoose.SchemaTypes.Url,
+      required: true,
+    },
+  },
+  {
+    timestamps: tr,
   }
 );
 
