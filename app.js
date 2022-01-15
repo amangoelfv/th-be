026daa894efeee6c2d5d77c1b5232a55bb44ff51
amangoelfv <amangoel300@gmail.com>
2022-01-15 21:09:37 +0530

    minor changes for auth connection

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 //routes
-const userRouter=require("./routes/userRoutes");
+const userRouter = require("./routes/userRoutes");
 const contestRouter = require("./routes/contests");
-const notifs=require("./routes/notifs")
-const leaderboard = require('./routes/getLeaderboard');
-const orderbook = require('./routes/getOrderbook');
+const notifs = require("./routes/notifs");
+const leaderboard = require("./routes/getLeaderboard");
+const orderbook = require("./routes/getOrderbook");
 const portfolio = require("./routes/getPortfolio");
 
 //middlewares
@@ -24,20 +24,22 @@ mongoose
   .catch((err) =>
     console.log("unable to conect to db because of error: ", err)
   );
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// parse application/json
 app.use(bodyParser.json());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Welcome to Trade Hunt");
+  res.send("Welcome to Trade Hunt");
 });
 
-app.use('/user', userRouter);
-app.use("/contests",auth, contestRouter);
-app.use('/leaderboard',auth, leaderboard);
-app.use('/orderbook',auth, orderbook);
-app.use('/portfolio',auth, portfolio);
-app.use("/news",auth,notifs);
+app.use("/user", userRouter);
+app.use("/contests", auth, contestRouter);
+app.use("/leaderboard", auth, leaderboard);
+app.use("/orderbook", auth, orderbook);
+app.use("/portfolio", auth, portfolio);
+app.use("/news", auth, notifs);
 
 app.listen(port, () => {
   console.log(`Tradehunt Backend is running at http://localhost:${port}`);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,63 +1,74 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const generateToken = require('../utils/generateToken');
-const User = require('../models/user');
+const generateToken = require("../utils/generateToken");
+const User = require("../models/user");
 
 const registerUser = async (req, res) => {
-     const { name, email, confirmPassword, password, } = req.body;
+  const { name, email, password, phone } = req.body;
+  console.log(name, email, password, phone);
+  try {
+    if (!(email && password && phone && name)) {
+      return res.status(400).send({
+        success: false,
+        message: "Please enter all fields",
+      });
+    }
+    const existingUser = await User.findOne({
+      $or: [{ email }, { phone }],
+    });
+    if (existingUser) {
+      console.log(existingUser);
+      let message = "E-mail already in use";
+      if (existingUser.phone === phone) message = "Phone already in use";
+      if (existingUser.phone === phone && existingUser.email === email)
+        message = "Phone & Email already in use";
+      return res.status(400).json({ message });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(name, email, phone, hashedPassword);
+    const user = new User({
+      name,
+      email,
+      phone,
+      password: hashedPassword,
+    });
+    await user.save();
+    const token = generateToken(user._id);
+    return res.status(200).json({ token });
+  } catch (err) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong", err });
+  }
+};
 
-     try {
-          if (!(email && password && confirmPassword && name)) {
-			res.status(400).send("Missing fields");
-          }
-          
-          const existingUser = await User.findOne({ email });
-		if (existingUser)
-			return res.status(400).json({ message: "User already exists" });
-          
-          if(password !== confirmPassword)
-               return res.status(400).json({ message: "Passwords do not match" });
-          
-          const salt=await bcrypt.genSalt(10);
-          const hashedPassword = await bcrypt.hash(password, salt);
-          const user = new User({
-               name,
-               email,
-               password: hashedPassword,
-          });
-          await user.save();
-          const token = generateToken(user._id);
-          res.status(200).json({ user, token });
-          
-     } catch (err) {
-           res.status(500).json({ message: "Something went wrong" });
-     }
-}
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    if (!(email && password)) {
+      res.status(400).send("Missing fields");
+    }
 
-const loginUser = async (req, res) => { 
-     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "User doesn't exist" });
 
-     try {
-          if (!(email && password)) {
-			res.status(400).send("Missing fields");
-          }
-          
-          const existingUser = await User.findOne({ email });
-          if (!existingUser)
-               return res.status(404).json({ message: "User doesn't exist" });
-          
-          const isPasswordValid = await bcrypt.compare(password, existingUser.password);
-          if (!isPasswordValid)
-               return res.status(401).json({ message: "Invalid password" });
-          
-          res.status(200).json({
-               user: existingUser,
-               token:generateToken(existingUser._id),
-          });
-     } catch (err) { 
-          res.status(500).json({ message: 'Something went wrong' });
-     }
-}
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid password" });
 
-module.exports = {registerUser, loginUser}
+    res.status(200).json({
+      user: existingUser,
+      token: generateToken(existingUser._id),
+    });
+  } catch (err) {
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
